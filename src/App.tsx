c589f594af5a636ab24324fdd038a136da293c3c
Lincoln Bunker <webdev.lincolnbunker@gmail.com>
2025-01-26 17:26:39 -0700

    configuring navbar bare bones

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import './App.css'
+// import './App.css'
 import { AppRoutes } from './routes'
 import { BrowserRouter } from 'react-router-dom'
+import { AppNavbar } from './components/Navbar'
 
 function App() {
 
   return (
     <React.Fragment>
       <BrowserRouter basename="/portfolio-lincoln">
+        <AppNavbar />
         <AppRoutes />
       </BrowserRouter>
     </React.Fragment>
