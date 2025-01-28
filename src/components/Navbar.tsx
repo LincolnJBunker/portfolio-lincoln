dabecff7e4a37e2155f0631a653e22888f6be188
Lincoln Bunker <webdev.lincolnbunker@gmail.com>
2025-01-27 21:40:35 -0700

    scrollable portoflio songit add .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,67 @@
+import { useRef } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+
+interface Ref {
+    current: any;
+}
 
 export const AppNavbar = () => {
-  const navigate = useNavigate();
+  // Create references for each section
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+  const resumeRef = useRef(null);
+
+  // Function to handle smooth scrolling
+  const handleScroll = (ref: Ref) => {
+    console.log('ref', ref)
+    ref.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand
-          onClick={() => navigate('/')}
-          className="fs-2 fw-bold" 
-        >
-          Lincoln Bunker
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link
-              onClick={() => navigate('/')}
-              className="fs-5 mx-4"
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => navigate('/about')}
-              className="fs-5 mx-4"
-            >
-              About
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => navigate('/projects')}
-              className="fs-5 mx-4"
-            >
-              Projects
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => navigate('/Contact')}
-              className="fs-5 mx-4"
-            >
-              Contact
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => navigate('/Resume')}
-              className="fs-5 mx-4"
-            >
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      {/* Navbar */}
+      <Navbar expand="lg" className="bg-body-tertiary sticky-top">
+        <Container>
+          <Navbar.Brand className="fs-2 fw-bold">Lincoln Bunker</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link onClick={() => handleScroll(aboutRef)} className="fs-5 mx-3">About</Nav.Link>
+              <Nav.Link onClick={() => handleScroll(projectsRef)} className="fs-5 mx-3">Projects</Nav.Link>
+              <Nav.Link onClick={() => handleScroll(contactRef)} className="fs-5 mx-3">Contact</Nav.Link>
+              <Nav.Link onClick={() => handleScroll(resumeRef)} className="fs-5 mx-3">Resume</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Page Content */}
+      <div>
+        {/* About Section */}
+        <section ref={aboutRef} style={{ height: "100vh", padding: "50px" }}>
+          <h1>About Me</h1>
+          <p>This is the About section.</p>
+        </section>
+
+        {/* Projects Section */}
+        <section ref={projectsRef} style={{ height: "100vh", padding: "50px" }}>
+          <h1>Projects</h1>
+          <p>This is the Projects section.</p>
+        </section>
+
+        {/* Contact Section */}
+        <section ref={contactRef} style={{ height: "100vh", padding: "50px" }}>
+          <h1>Contact</h1>
+          <p>This is the Contact section.</p>
+        </section>
+
+        {/* Resume Section */}
+        <section ref={resumeRef} style={{ height: "100vh", padding: "50px" }}>
+          <h1>Resume</h1>
+          <p>This is the Resume section.</p>
+        </section>
+      </div>
+    </>
   );
 };
