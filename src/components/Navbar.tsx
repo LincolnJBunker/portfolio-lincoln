16317126eb3cde582fad9c0b5aa2496817b62296
Lincoln Bunker <webdev.lincolnbunker@gmail.com>
2025-01-27 21:29:37 -0700

    refactoring sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,32 +2,53 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 export const AppNavbar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand onClick={() => navigate('/')}>Lincoln Bunker</Navbar.Brand>
+        <Navbar.Brand
+          onClick={() => navigate('/')}
+          className="fs-2 fw-bold" 
+        >
+          Lincoln Bunker
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate('/about')}>About</Nav.Link>
-            <Nav.Link onClick={() => navigate('/projects')}>Projects</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+          <Nav className="ms-auto">
+            <Nav.Link
+              onClick={() => navigate('/')}
+              className="fs-5 mx-4"
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => navigate('/about')}
+              className="fs-5 mx-4"
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => navigate('/projects')}
+              className="fs-5 mx-4"
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => navigate('/Contact')}
+              className="fs-5 mx-4"
+            >
+              Contact
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => navigate('/Resume')}
+              className="fs-5 mx-4"
+            >
+              Resume
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
