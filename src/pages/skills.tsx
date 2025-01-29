1507c7f04a4856ee9be7891174bdc188659c1276
Lincoln Bunker <webdev.lincolnbunker@gmail.com>
2025-01-29 16:53:46 -0700

    adding skills arrays

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -2,10 +2,63 @@ import React from "react";
 
 export const Skills = () => {
 
+    const frontend = [
+        'Javascript',
+        'Typescript',
+        'React',
+        'Redux',
+        'HTML',
+        'CSS',
+        'Bootstrap',
+        'Material UI',
+    ];
+
+    const backend = [
+        'Python',
+        'PostgresSQL',
+        'MySQL',
+        'Java',
+        'Node.js',
+        'Express',
+        'REST API',
+        'ETL',
+        'Data Pipelines',
+        'Bash'
+    ];
+
+    const tools = [
+        'Git',
+        'GitHub',
+        'Postman',
+        'Axios',
+        'VSCode',
+        'Microsoft Windows',
+        'Mac OS',
+        'Linux'
+    ];
+
     return (
         <React.Fragment>
             <div>
-                <h1>Skills</h1>
+                <div 
+                style={{
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                    display: 'flex'
+                }}>
+                    <h1>Skills</h1>
+                </div>
+                <div>
+                    <div>
+                        <h3>Front End</h3>        
+                    </div>
+                    <div>
+                        <h3>Back End</h3>
+                    </div>
+                    <div>
+                        <h3>Tools / Cloud</h3>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     )
