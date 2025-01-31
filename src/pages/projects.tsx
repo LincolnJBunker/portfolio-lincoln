336e609b744295e068fab4b32de51fde87efccc6
Lincoln Bunker <webdev.lincolnbunker@gmail.com>
2025-01-31 00:09:16 -0700

    sentinel fix

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sentinel from '../../public/Sentinel.png'
+// import Sentinel from '../../public/Sentinel.png'
 import ATPM from '../../public/ATPM.jpeg'
 import NatureNetwork from '../../public/NatureNetwork.jpeg'
 import Huzz from '../../public/HUZZ.png'
@@ -11,7 +11,7 @@ export const Projects = () => {
             tech: "Typescript, React, MUI, Python, PostgreSQL, AWS, Serverless",
             description: `While working at Monolith, I helped collaborate and develop Sentinel, which handles real-time Amazon data for each of Monolith's brands.
             I contributed significantly to the second version of the app, adding admin features for managing FBM, WFM, Shopify Orders, feature flag integrations, and company reports.`,
-            image: Sentinel
+            // image: Sentinel
         },
         {
             title: "Alpine Twin Peak Maintenance",
@@ -42,7 +42,10 @@ export const Projects = () => {
                 <h1>Projects</h1>
                 {projects.map((project, index) => (
                     <div key={index} className="project">
+                        {project.image &&
                         <img src={project.image} alt={project.title} className="project-image" />
+
+                        }
                         <div className="project-details">
                             <h3>{project.title}</h3>
                             <h5 className="project-tech">{project.tech}</h5>
